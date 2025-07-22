9af292f6be5e3075a405d2d2f120e167a14dfb50
Anmolvaish0708 <anmolvaishnaw0708@gmail.com>
2025-07-22 20:11:54 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  //  const[firstName, setFirstName] = useState("");
+  //  const[lastName,setLastName] = useState("");
+
+
+  // function changeFirstNameHandler(event){
+  //   // console.log("printing first name")
+  //   // console.log(event.target.value)
+  //   setFirstName(event.target.value)
+  // }
+
+  // function changeLastNameHandler(event){
+  //   // console.log("printing last name")
+  //   // console.log(event.target.value)
+  //   setLastName(event.target.value)
+  // }
+
+  const[formData,setFormData] = useState( {firstName:"", lastName:"", comments:"", inVisible: true, mode:"", favCar:""});
+  console.log(formData)
+
+  function changeHandler(event) {
+    const {name,value,checked,type} = event.target
+     setFormData( prevFormData =>{
+      return {
+        ...prevFormData, [name]: type ==="checkbox" ? checked: value
+      }
+     })
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    console.log("printing the form data......");
+    console.log(formData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="first name"
+        onChange={changeHandler} name="firstName" value={formData.firstName}/> 
+        <br/>
+        <br/>
+    
+       <input type="text" placeholder="last name"
+        onChange={changeHandler} name="lastName" value={formData.lastName}/> 
+
+        <br/>
+        <br/>
+
+        <textarea
+        placeholder='enter comments' onChange={changeHandler} name="comments" value={formData.comments}
+        />
+        <br/>
+        <br/>
+
+        <input type="checkbox" name='inVisible' id='inVisible' onChange={changeHandler} checked = {formData.inVisible}/>
+        <label htmlFor='inVisible'>inVisible?</label>
+
+        <br/>
+        <br/>
+
+        <input type="radio" onChange={changeHandler} name="mode" value="online mode" id="online mode" checked = {formData.mode === "online mode"}/>
+        <label htmlFor='online mode'>online mode</label>
+
+        <br/>
+        <br/>
+
+        <input type="radio" onChange={changeHandler} name="mode" value="offline mode" id="offline mode" checked = {formData.mode === "offline mode"}/>
+        <label htmlFor='online mode'>offline mode</label>
+
+        <br/>
+        <br/>
+        
+        <label htmlFor='favCar'>Favourite Car</label>
+        <select name='favCar' id='favCar' value={formData.favCar} onChange={changeHandler}>
+            <option value="scorpio">Scorpio</option>
+            <option value="fortuner">fortunor</option>
+            <option value="brezza">brezza</option>
+            <option value="swift">swift</option>
+            <option value="defender">defender</option>
+        </select>
+       <br/>
+       <br/>
+
+       <button>submit</button>
+      </form>
     </div>
   );
 }
